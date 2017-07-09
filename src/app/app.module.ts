fb35def125ae554083c4cc476968a1ca985ee236
leobarnabas <leobarnabas@gmail.com>
2017-07-09 18:27:34 -0400

    Merging bootstrap changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
-import {RouterModule, Routes} from "@angular/router";
+import {RouterModule} from "@angular/router";
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
 import {SignInComponent} from "./sign-in/sign-in.component";
