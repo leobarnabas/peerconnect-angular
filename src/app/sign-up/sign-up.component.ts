c0d1d3f9dd7fd08ccb7ee0c940bac3d65eaf18df
leobarnabas <leobarnabas@gmail.com>
2017-08-20 19:16:14 -0400

    Sign up user changes

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {Headers, Http} from "@angular/http";
 
 @Component({
   selector: 'app-sign-up',
@@ -7,10 +8,33 @@ import {Component, OnInit} from "@angular/core";
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() {
+  headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) {
   }
 
   ngOnInit() {
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+
+    var user = {
+      "firstName": e.target.elements[0].value,
+      "lastName": e.target.elements[1].value,
+      "dateOfBirth": null,
+      "id": e.target.elements[9].value,
+      "password": e.target.elements[10].value,
+      "gender": e.target.elements[7].value,
+      "area": e.target.elements[5].value,
+      "field": e.target.elements[6].value,
+      "meetingPreference": e.target.elements[8].value,
+      "isMentor": null
+    };
+    this.http.post('/users' , JSON.stringify(user), {headers: this.headers}).subscribe(data => {
+
+      console.log('data after save:', data);
+
+    });
+  }
 }
